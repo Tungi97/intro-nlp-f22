a9a94ceddf7d68668316939b4576f634a9c77285
Tung Nguyen <tung.nguyen@adnovum.ch>
2022-09-28 16:39:50 +0200

    operations implemented

--- assignment_1/backprop/operations.py
+++ assignment_1/backprop/operations.py
@@ -27,10 +27,10 @@ class Log(Operator):
     ## natural logarithm
 
     def f(self, a, b = None):
-        pass ## ToDO: implement
+        return math.log(a)
 
     def df(self, a, b = None):
-        pass ## ToDO: implement
+        return [1 / a]
 
 
 class Mult(Operator):
@@ -45,27 +45,34 @@ class Mult(Operator):
 class Div(Operator):
 
     def f(self, a, b):
-        pass ## ToDO: implement
+        return a / b
 
     def df(self, a, b):
-        pass ## ToDO: implement
+        return [1 / b, -a / b**2]
 
 class Add(Operator):
 
     def f(self, a, b):
-        pass ## ToDO: implement
+        return a + b
 
     def df(self, a, b = None):
-        pass ## ToDO: implement
+        return [1, 1]
+
 
 
 class Sub(Operator):
 
     def f(self, a, b = None):
-        pass ## ToDO: implement
+        if isinstance(b, None):
+            return -a
+        else:
+            return a - b
 
     def df(self, a, b = None):
-        pass ## ToDO: implement
+        if isinstance(b, None):
+            return [-1]
+        else: 
+            return [1, -1]
 
 
 class Pow(Operator):
@@ -76,26 +83,28 @@ class Pow(Operator):
     def df(self, a, b):
         if a <= 0: ## work-around: treat as unary operation if -a^b
             return [b * (a ** (b - 1))]
+ 
         else:
             return [b * (a ** (b - 1)), (a ** b) * math.log(a)]
+
 
 
 class Sin(Operator):
 
     def f(self, a, b=None):
-        pass ## ToDO: implement
+        return math.sin(a)
 
     def df(self, a, b=None):
-        pass ## ToDO: implement
+        return [math.cos(a)]
 
 
 class Cos(Operator):
 
     def f(self, a, b=None):
-        pass ## ToDO: implement
+        return math.cos(a)
 
     def df(self, a, b=None):
-        pass ## ToDO: implement
+        return [-math.sin(a)]
 
 
 
